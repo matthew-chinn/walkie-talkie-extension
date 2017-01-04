5fb77715d33477f463f9a077f9008c3f6ad1989d
Matthew Chinn <mattchinn96@gmail.com>
2017-01-04 14:04:22 -0800

    first commit

---
+++ popup.js
@@ -0,0 +1,35 @@
+function getPage(url, method) {
+    var x = new XMLHttpRequest();
+    x.open(method, url);
+    //x.responseType = 'json';
+    x.responseType = 'document';
+    x.onload = function() {
+        // Parse and process the response from Google Image Search.
+        html = x.response.children[0];
+        body = html.children[1];
+        container = body.children[0];
+        renderHtml(container)
+            return;
+    };
+    x.onerror = function() {
+        renderHtml('<p>Network error.</p>');
+        return;
+    };
+    x.send();
+
+}
+
+function renderHtml(html) {
+    $('div#content').html(html);
+    $('div#content a').bind("click", function(){
+        link = $(this).attr('href');
+        method = $(this).attr('data-method');
+        if(method == null)
+            method = "GET";
+        getPage(link, method);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getPage("http://localhost:3000", 'GET');
+});
